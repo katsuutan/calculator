6a67231ed532b51dd5344183cacfed08bd60604c
Jason <katsuutan0408@gmail.com>
2023-11-28 10:09:35 -0800

    Create js functions, basic html and css layout

---
+++ calculator.js
@@ -0,0 +1,34 @@
+const add = (a, b) => {
+    return a + b;
+};
+
+const subtract = (a, b) => {
+    return a - b;
+};
+
+const multiply = (a, b) => {
+    return a * b;
+};
+
+const divide = (a, b) => {
+    return a / b;
+};
+
+let firstNum,
+    secondNum,
+    operator;
+
+
+const operate = (a, op, b) => {
+    switch (op) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract (a, b);
+        case '*':
+            return multiply (a, b);
+        case '/':
+            return divide (a, b);
+    }
+    return 'Error: operate() operator invalid';
+};
